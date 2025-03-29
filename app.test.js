12fb8b6795190af92a814698313a513a0f0146fe
Saturnnnnnnnnnnn <hamza.shermatov0504@gmail.com>
2025-03-29 22:00:44 +0300

    arrrrrrrgo

--- app.test.js
+++ app.test.js
@@ -9,7 +9,7 @@ describe('Тестирование HTML-ответа', () => {
 
   it('должен содержать заголовок "Информация о системе"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toContain('<h1>Информация о системе</h1>');
+    expect(response.text).toContain('<h1>Информация о вашей системе</h1>');
   });
 
   it('должен содержать информацию о хосте', async () => {
